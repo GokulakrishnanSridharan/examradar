26cdb757b835229c2a3bc58a4cb43da69c730192
GokulakrishnanSridharan <gokulakrishnansridharan@gmail.com>
2026-05-28 09:42:53 +0530

    Fix requirements filename

--- data/database.py
+++ data/database.py
@@ -1,12 +1,16 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 from datetime import datetime
 
+load_dotenv()
 def get_connection():
     conn = psycopg2.connect(
-        host="localhost",
-        database="examradar",
-        user="postgres",
-        password="root"  # replace with your actual password
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
     return conn
 
